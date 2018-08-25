5d290f451abcb00ed3c87046e9995c55d13a0086
Borzy <borzy@MacBook-Pro-Borzy.local>
2018-08-25 20:28:07 +0300

    two print statements

--- GitDemo/GitDemo/ViewController.swift
+++ GitDemo/GitDemo/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Bug fix")
+        print("Second line")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
